816aaddba4f1aa05501329c868bd34906f03821b
phyoeitheint <phyoeitheint851@gmail.com>
2024-06-04 15:24:54 +0630

    supervisor

---
+++ resources/views/backend/supervisors/create.blade.php
@@ -0,0 +1,74 @@
+@extends('backend.layouts.app')
+@section('content')
+<div class="wrapper">
+    <div class="content-wrapper">
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12 mt-3">
+                        <!-- general form elements -->
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">Create New Supervisor</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <!-- form start -->
+                            <form action="{{ route('supervisors.store') }}" method="POST">
+                                @csrf
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label class="form-label" for="name">Name</label>
+                                        <input type="text" name="name" class="form-control" id="name" placeholder="Enter Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="email">Email</label>
+                                        <input type="text" name="email" class="form-control" id="email" placeholder="Enter Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="password">Password</label>
+                                        <input type="text" name="password" class="form-control" id="password" placeholder="Enter Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="phone">Phone</label>
+                                        <input type="text" name="phone" class="form-control" id="phone" placeholder="Enter Phone">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="position">Position</label>
+                                        <input type="text" name="position" class="form-control" id="position" placeholder="Enter Position">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="address">Address</label>
+                                        <textarea class="form-control" name="address" id="address" rows="3"></textarea>
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+
+        {{-- Form Start Here --}}
+    </div>
+</div>
+
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+        $(document).ready(function (){
+            $('.select-role').select2({
+                theme: 'bootstrap4'
+            })
+        });
+    </script>
+@endsection
+
